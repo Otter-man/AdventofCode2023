95d667de0f31525ff8225c160a5cb202749475d8
Danil Tereshchenko <danil.tereshchenko@jetbrains.com>
2023-12-08 23:40:42 +0400

    Day 7 part 2 solved

--- day_7.py
+++ day_7.py
@@ -10,14 +10,14 @@ with open('inputs/day_7_test.txt') as f:
 
 @dataclass
 class Hand:
-    card_1: tuple
-    card_2: tuple
-    card_3: tuple
-    card_4: tuple
-    card_5: tuple
+    card_1: int
+    card_2: int
+    card_3: int
+    card_4: int
+    card_5: int
     bet: int
     combination: int
-    card_weights = {'A': 14, 'K': 13, 'Q': 12, 'J': 11,
+    card_weights = {'A': 14, 'K': 13, 'Q': 12, 'J': 1,
                     'T': 10, '9': 9, '8': 8, '7': 7,
                     '6': 6, '5': 5, '4': 4, '3': 3,
                     '2': 2}
@@ -30,34 +30,49 @@ class Hand:
         self.card_3 = self.card_weights.get(cards[2].upper(), 0)
         self.card_4 = self.card_weights.get(cards[3].upper(), 0)
         self.card_5 = self.card_weights.get(cards[4].upper(), 0)
+        self.all_cards = [self.card_1, self.card_2, self.card_3, self.card_4, self.card_5]
         self.combination = self._determine_combination()
 
     def _determine_combination(self):
-        card_counts = Counter(card for card in [self.card_1, self.card_2, self.card_3, self.card_4, self.card_5])
+        card_counts = Counter(card for card in self.all_cards)
         count_frequencies = list(card_counts.values())
         count_frequencies.sort(reverse=True)
+        joker_count = sum(card == 1 for card in self.all_cards)
 
         if count_frequencies[0] == 5:
             return 6  # Five of a kind
         elif count_frequencies[0] == 4:
+            if joker_count == 1 or joker_count == 4:
+                return 6
             return 5  # Four of a kind
         elif count_frequencies[0] == 3 and count_frequencies[1] == 2:
+            if joker_count == 2 or joker_count == 3:
+                return 6
             return 4  # Full house
         elif count_frequencies[0] == 3:
+            if joker_count == 1 or joker_count == 3:
+                return 5
             return 3  # Three of a kind
         elif count_frequencies[0] == 2 and count_frequencies[1] == 2:
+            if joker_count == 1:
+                return 4
+            if joker_count == 2:
+                return 5
             return 2  # Two pairs
         elif count_frequencies[0] == 2:
+            if joker_count == 1 or joker_count == 2:
+                return 3
             return 1  # One pair
         else:
+            if joker_count == 1:
+                return 1
             return 0  # High card
 
     def __lt__(self, other):
         if self.combination < other.combination:
             return True
         elif self.combination == other.combination:
-            for card_self, card_other in zip([self.card_1, self.card_2, self.card_3, self.card_4, self.card_5],
-                                             [other.card_1, other.card_2, other.card_3, other.card_4, other.card_5]):
+            for card_self, card_other in zip(self.all_cards, other.all_cards):
                 if card_self != card_other:
                     return card_self < card_other
         return False
@@ -70,6 +85,5 @@ if __name__ == "__main__":
 
     sorted_hand_objects = sorted(all_hands, reverse=False)
 
-
-    part_1 = sum([hand.bet * (c+1) for c, hand in enumerate(sorted_hand_objects)])
-    print(part_1)
+    part_2 = sum([hand.bet * (c+1) for c, hand in enumerate(sorted_hand_objects)])
+    print(part_2)
